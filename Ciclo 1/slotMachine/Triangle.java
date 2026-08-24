e33e0f25ead612e85175f829fdb2d05506af318b
Anonimo2007 <gnfrnc97@gmail.com>
2026-08-23 23:00:56 -0500

    Clean up comments in Triangle.java
    Removed unnecessary comments from the Triangle class methods.

--- Ciclo 1/slotMachine/Triangle.java
+++ Ciclo 1/slotMachine/Triangle.java
@@ -18,9 +18,7 @@ public class Triangle{
     private String color;
     private boolean isVisible;
 
-    /**
-     * Create a new triangle at default position with default color.
-     */
+
     public Triangle(){
         height = 30;
         width = 40;
@@ -30,74 +28,53 @@ public class Triangle{
         isVisible = false;
     }
 
-    /**
-     * Make this triangle visible. If it was already visible, do nothing.
-     */
+
     public void makeVisible(){
         isVisible = true;
         draw();
     }
     
-    /**
-     * Make this triangle invisible. If it was already invisible, do nothing.
-     */
+
     public void makeInvisible(){
         erase();
         isVisible = false;
     }
     
-    /**
-     * Move the triangle a few pixels to the right.
-     */
+
     public void moveRight(){
         moveHorizontal(20);
     }
 
-    /**
-     * Move the triangle a few pixels to the left.
-     */
+
     public void moveLeft(){
         moveHorizontal(-20);
     }
 
-    /**
-     * Move the triangle a few pixels up.
-     */
+
     public void moveUp(){
         moveVertical(-20);
     }
 
-    /**
-     * Move the triangle a few pixels down.
-     */
+
     public void moveDown(){
         moveVertical(20);
     }
 
-    /**
-     * Move the triangle horizontally.
-     * @param distance the desired distance in pixels
-     */
+
     public void moveHorizontal(int distance){
         erase();
         xPosition += distance;
         draw();
     }
 
-    /**
-     * Move the triangle vertically.
-     * @param distance the desired distance in pixels
-     */
+
     public void moveVertical(int distance){
         erase();
         yPosition += distance;
         draw();
     }
 
-    /**
-     * Slowly move the triangle horizontally.
-     * @param distance the desired distance in pixels
-     */
+
     public void slowMoveHorizontal(int distance){
         int delta;
 
@@ -114,10 +91,7 @@ public class Triangle{
         }
     }
 
-    /**
-     * Slowly move the triangle vertically.
-     * @param distance the desired distance in pixels
-     */
+
     public void slowMoveVertical(int distance){
         int delta;
 
@@ -134,11 +108,7 @@ public class Triangle{
         }
     }
 
-    /**
-     * Change the size to the new size
-     * @param newHeight the new height in pixels. newHeight must be >=0.
-     * @param newWidht the new width in pixels. newWidht must be >=0.
-     */
+
     public void changeSize(int newHeight, int newWidth) {
         erase();
         height = newHeight;
@@ -146,19 +116,13 @@ public class Triangle{
         draw();
     }
     
-    /**
-     * Change the color. 
-     * @param color the new color. Valid colors are "red", "yellow", "blue", "green",
-     * "magenta" and "black".
-     */
+
     public void changeColor(String newColor){
         color = newColor;
         draw();
     }
 
-    /*
-     * Draw the triangle with current specifications on screen.
-     */
+
     private void draw(){
         if(isVisible) {
             Canvas canvas = Canvas.getCanvas();
@@ -169,9 +133,7 @@ public class Triangle{
         }
     }
 
-    /*
-     * Erase the triangle on screen.
-     */
+
     private void erase(){
         if(isVisible) {
             Canvas canvas = Canvas.getCanvas();
