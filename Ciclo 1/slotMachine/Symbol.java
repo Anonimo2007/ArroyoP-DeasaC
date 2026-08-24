6b71c6ea3c0500d0372be6fde71242d2330b293a
Anonimo2007 <gnfrnc97@gmail.com>
2026-08-23 22:59:53 -0500

    Clean up comments in Symbol.java
    Removed unnecessary comments and documentation.

--- Ciclo 1/slotMachine/Symbol.java
+++ Ciclo 1/slotMachine/Symbol.java
@@ -1,7 +1,5 @@
 /**
  * A Symbol is a token identified only by its color (a CSS3 color name).
- * The same catalog of symbols can be placed on any wheel of the machine.
- *
  * @author  Gian Franco Arroyo Perez, Nicolas Deaza Casasbuenas
  * @version 1.0
  */
@@ -9,18 +7,12 @@ public class Symbol{
 
     private String color;
 
-    /**
-     * Create a symbol with the given (already validated) CSS color name.
-     * @param color a valid CSS3 color name
-     */
+
     public Symbol(String color){
         this.color = color;
     }
 
-    /**
-     * Return this symbol's color name.
-     * @return the CSS3 color name identifying this symbol
-     */
+
     public String getColor(){
         return color;
     }
